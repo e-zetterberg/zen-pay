2b171d1c802319c06223b51b509af8154964426b
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-18 15:46:00 +0100

    initialized client and server folders, wohoo

---
+++ server/src/main/java/salt/se/jfs/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package salt.se.jfs.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
