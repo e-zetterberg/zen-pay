1862b82f6cf6ffeefe88924fa4a27593b619d67d
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-21 22:00:29 +0100

    fetch balance from back-end

--- client/app/account/layout.jsx
+++ client/app/account/layout.jsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 
-export default function AccountLayout({ children }) {
+export default async function AccountLayout({ children }) {
+
+    const fetchBalance = async () => {
+        const response = await fetch('http://localhost:8080/api/accounts/1')
+        const data = await response.json();
+        return data.balance;
+    };
+   const balance = await fetchBalance();
   return (
     
       <main className="main homepage--balance">
         <section className="balance--container">
-        <div className="balance--display">1000 kr</div>
+        <div className="balance--display">{balance} kr</div>
         <div className="balance--button-container">
           <Link href="/account/deposit">
             <button className="btn btn--deposit">Deposit</button>
