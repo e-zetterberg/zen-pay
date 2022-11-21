902cb000361356596eadb785f15246ac75ffa7da
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-21 15:26:53 +0100

    made InputAmount a client component

--- client/app/account/InputAmount.jsx
+++ client/app/account/InputAmount.jsx
@@ -1,12 +1,32 @@
 "use client";
 
+import { useState } from 'react';
 import React from 'react'
 import "../../styles/InputAmount.css"
 
 const InputAmount = () => {
+
+    const [amount, setAmount] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(amount);
+
+        //send in fetch function as prop and call it here
+        //... probably :P
+        setAmount("")
+    };
+
   return (
-    <form className='input-form'>
-      <input className='input-field' type="text" />
+    <form onSubmit={e => handleSubmit(e)} className='input-form'>
+      <input 
+        onChange={e => setAmount(e.target.value)}
+        required={true}
+        value={amount}
+        className='input-field' 
+        type="text" 
+        placeholder='Enter Amount'
+      />
       <button className='btn btn--confirm' type="submit">Confirm</button>
     </form>
   )
