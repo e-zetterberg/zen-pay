e738f66951ac21fb11376d3aec57091b99738bf2
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-22 18:08:20 +0100

    implemented transactions

--- client/app/account/layout.jsx
+++ client/app/account/layout.jsx
@@ -8,14 +8,14 @@ export default async function AccountLayout({ children }) {
           cache: 'no-store',
         });
         const data = await response.json();
-        return data.balance;
+        return data;
     };
-   const balance = await fetchBalance();
+   const data = await fetchBalance();
   return (
     
       <main className="main homepage--balance">
         <section className="balance--container">
-        <div className="balance--display">{balance} kr</div>
+        <div className="balance--display">{data.balance} kr</div>
         <div className="balance--button-container">
           <Link href="/account/deposit">
             <button className="btn btn--deposit">Deposit</button>
@@ -25,6 +25,18 @@ export default async function AccountLayout({ children }) {
           </Link>
         </div>
             {children}
+        <div className="transaction-container">
+          <h3>Transactions</h3>
+          <hr />
+          <ul>
+            {data.transactions.map(tx => (
+              <li key={tx.transactionId}>
+                <span>{tx.description} </span>
+                <span> {tx.amount}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
         </section>
       </main>
     
