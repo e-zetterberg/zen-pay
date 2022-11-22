3ceeb0d1273bea7d19099f0bea93b604a3c7c739
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2022-11-22 21:30:06 +0100

    menu UI fixed, routing pending

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,32 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
-import '../styles/Header.css'
+import "../styles/Header.css"
+import Menu from "./Menu"
 
-const Header = () => {
+function Header() {
   return (
-    <header className='header'>
-        <nav className='header--navbar'>
-            <Link href="/login">
-            <div className='navbar--logo'>
-                <Image src="/../public/logo-transparent.png" alt="Logo" width={211} height={73}></Image>
-            </div>
-            </Link>
-
-            <section className='navbar-menu'>
-              <Link href="/details">
-            <div className='navbar--user'>
-                <Image src="/../public/profile.png" alt="Logo" width={55} height={53}></Image>
-            </div>
-              </Link>
-            <Link href="/account">
-              <div className='navbar--burger'>
-                  &#9776;
-              </div> 
-            </Link>
-            </section>
-        </nav>
-    </header>
+    <div className="header--container">
+        <Menu className="header--menu"/>
+        <Image className="header--logo" src="/logo-transparent.png" alt="Logo" width={211} height={73}></Image>
+        <span className="align--span"></span>
+    </div>
   )
 }
 
