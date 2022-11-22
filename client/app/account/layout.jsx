94c90f66364b0c954d6a2c3fbaeffa95cb3c4ed0
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-22 12:11:20 +0100

    auth now locks you out if you're not logged in

--- client/app/account/layout.jsx
+++ client/app/account/layout.jsx
@@ -1,22 +1,7 @@
 import Link from "next/link";
 
+
 export default async function AccountLayout({ children }) {
-
-    const getSession = async (cookie) => {
-      const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/session`, {
-        headers: { cookie },
-      })
-      if (!response?.ok) {
-        return null
-      }
-      const session = await response.json()
-      return Object.keys(session).length > 0 ? session : null
-    };
-
-    const session = await getSession();
-    if(!session){
-      console.log('yuo ar not alowed');
-    }
 
     const fetchBalance = async () => {
         const response = await fetch('http://localhost:8080/api/accounts/1', {
