7746affa72ebc8d6403a7d220f59a8e52404d92e
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-22 10:53:00 +0100

    testing out auth things

--- client/app/account/layout.jsx
+++ client/app/account/layout.jsx
@@ -2,8 +2,26 @@ import Link from "next/link";
 
 export default async function AccountLayout({ children }) {
 
+    const getSession = async (cookie) => {
+      const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/session`, {
+        headers: { cookie },
+      })
+      if (!response?.ok) {
+        return null
+      }
+      const session = await response.json()
+      return Object.keys(session).length > 0 ? session : null
+    };
+
+    const session = await getSession();
+    if(!session){
+      console.log('yuo ar not alowed');
+    }
+
     const fetchBalance = async () => {
-        const response = await fetch('http://localhost:8080/api/accounts/1')
+        const response = await fetch('http://localhost:8080/api/accounts/1', {
+          cache: 'no-store',
+        });
         const data = await response.json();
         return data.balance;
     };
