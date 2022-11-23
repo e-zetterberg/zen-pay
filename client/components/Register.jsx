af8e7a760670d0cff2d0c480967b27d3d6d712e1
salt-dev <swetha.chauhan0001@gmail.com>
2022-11-23 09:05:34 +0100

    add form

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
+'use client';
+import React, { useState } from 'react'
+import { useSession } from "next-auth/react"
+ 
 function Register() {
-  return <div>Register</div>;
+  const { data: session } = useSession();
+
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState(session ? session.user.email : "")
+    const handleSubmit = () => {
+      console.log(name, phone, email)
+    }
+    return (
+        <div className="form--container">
+            <form className=""  onSubmit={()=> handleSubmit()}>
+                <input 
+                type='text' 
+                placeholder='Name'
+                value={name}
+                size="30"
+                onChange={(e)=>setName(e.target.value)}/>
+                 <br/>
+                <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                pattern=".+@globex\.com" size="30" required
+                />
+                 <br/>
+                <input 
+                type="tel"
+                name="phone"
+                placeholder="phone"
+                size="30"
+                value={phone}
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                required />
+                 <br/>
+                <input type="submit" />
+
+            
+            </form>
+        </div>
+  )
 }
 
-export default Register;
+export default Register
