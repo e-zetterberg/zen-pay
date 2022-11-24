6b97e5f762d1f30b679b5d1ad5cfee3b1392a500
salt-dev <swetha.chauhan0001@gmail.com>
2022-11-24 16:29:59 +0100

    user context made available in root layout

--- client/app/(auth)/login/page.jsx
+++ client/app/(auth)/login/page.jsx
@@ -2,18 +2,24 @@
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Register from "../../../components/Register";
+import { useUserContext } from "../../../components/UserContext";
+import Details from "../../details/page";
 
 const Login = () => {
   const { data: session } = useSession();
-
+  const {userData} = useUserContext();
+  console.log(userData);
   return (
     <main className="main login">
       <div></div>
-   
-        {/* {session ? `Hello ${session.user.name}` : "Hello user, please sign in"} */}
-        {session ? <Register /> : "Hello user, please sign in"}
+        
+        {session?
+          userData? <Details/> : <Register />
+          : "Hello user, please sign in"
+        }
  
       {!session ? (
+       
         <button onClick={signIn}>Sign In</button>
       ) : (
         <button onClick={signOut}>Sign Out</button>
