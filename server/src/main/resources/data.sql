f27b415e3af16e3a3482be0935e1a27e787a79bf
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-24 11:28:36 +0100

    updated data seeding

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO public.users (user_id, created_on, email, name, phone) VALUES (1, '2022-11-23T14:39:19.568Z', 'swetha.sureshmal@appliedtechnology.se', 'Swetha', '0908038108');
-INSERT INTO public.users (user_id, created_on, email, name, phone) VALUES (2, '2022-11-23T14:39:19.568Z', 'erik.zetterberg@appliedtechnology.se', 'Erik', '0908038108');
+INSERT INTO public.users (user_id, created_on, email, name, phone) VALUES (2, '2022-11-23T14:39:19.568Z', 'erik.zetterberg@appliedtechnology.se', 'Erik', '0735099472');
+INSERT INTO public.users (user_id, created_on, email, name, phone) VALUES (3, '2022-11-23T14:39:19.568Z', 'andrei.varzari@appliedtechnology.se', 'Andrei', '0908038108');
 
 INSERT INTO public.account(account_id, balance, fk_user_id) VALUES (1873771645899017, 0, 1);
 INSERT INTO public.account(account_id, balance, fk_user_id) VALUES (5184738024134449, 800, 2);
-INSERT INTO public.account(account_id, balance, fk_user_id) VALUES (5184738024134449, 800, 2);
+INSERT INTO public.account(account_id, balance, fk_user_id) VALUES (1452179495900473, 2000, 3);
