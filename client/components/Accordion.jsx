f8ad32ee01832c59c49fda2b34894540e4c1d5c8
varzantr <vrz.andrei@gmail.com>
2022-11-26 16:06:06 +0100

    about page created

--- client/components/Accordion.jsx
+++ client/components/Accordion.jsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const SimpleAccordion = () => (
