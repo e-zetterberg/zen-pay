462b255c33a16d5b8a2391250053670f22ad191e
varzantr <vrz.andrei@gmail.com>
2022-11-29 10:32:09 +0100

    register page edited to add welcome text

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -5,7 +5,10 @@ import '../styles/Register.css';
 
 const Register = () => (
   <main className="main create-account-container">
-    <div className="create-account-text">Create your ZenPay account</div>
+    <div className="both-text">
+      <div className="welcome-text">Welcome to ZenPay!</div>
+      <div className="create-account-text">Create a ZenPay account to continue</div>
+    </div>
     <Image src="/../public/create-account.png" alt="create-account" width={260} height={250} />
     <Link href="/register">
       <button type="button" className="create-account-btn">Register</button>
