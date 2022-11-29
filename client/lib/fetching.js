7b00838d5d278128b70fc6ec75f1dcf61564999b
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-29 20:00:46 +0100

    merged prod to main

--- client/lib/fetching.js
+++ client/lib/fetching.js
@@ -1,4 +1,4 @@
-export const baseApiPath = 'https://zenpay.azurewebsites.net/api';
+export const baseApiPath = 'http://localhost:8080/api';
 export const devApiPath = 'http://localhost:8080/api';
 export const prodApiPath = 'https://zenpay.azurewebsites.net/api';
 
