147611f1e225c749f06321ee06228f2054fb979c
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-30 17:07:11 +0100

    change about pic

--- client/app/about/page.jsx
+++ client/app/about/page.jsx
@@ -8,7 +8,7 @@ const About = () => (
     <main className="main main--container">
       <div className="about--about">About us</div>
       <div className="about--container">
-        <Image className="about--image" src="/aboutpic.png" alt="about-us" width={360} height={250} />
+        <Image className="about--image" src="/string-quartet.png" alt="about-us" width={360} height={280} />
         <p className="about--description">
           Our mission is to unlock the power of a borderless economy, for everyone.
           Financial services are the backbone of our society, and our goal is to make
