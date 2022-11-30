398c00665bf6741486b861c37a08374b658f972f
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-11-30 11:45:16 +0100

    configured apipath for prod

--- client/lib/apiPath.js
+++ client/lib/apiPath.js
@@ -1,3 +1,3 @@
-export const baseApiPath = 'http://localhost:8080/api';
+export const baseApiPath = 'https://zenpay.azurewebsites.net/api';
 export const devApiPath = 'http://localhost:8080/api';
 export const prodApiPath = 'https://zenpay.azurewebsites.net/api';
