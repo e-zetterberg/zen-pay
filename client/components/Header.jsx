4d99a035214ed5116e89a75fbb700b91a92ab781
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2022-12-02 13:24:04 +0100

    fixed dashboard wallet balance not updating

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import '../styles/Header.css';
 import Menu from './Menu';
+import DashboardLink from './dashboard/DashboardLink';
+import { getSession } from '../lib/session';
 
-const Header = () => (
-  <header className="header">
-    <nav className="header--navbar">
-      <span className="align--span" />
-      <Link href="/">
-        <Image priority className="header--logo" src="/zen-red-logo.png" alt="Logo" width={240} height={66} />
-      </Link>
-      <Menu className="header--menu" />
-    </nav>
-  </header>
-);
+const Header = () => {
+  const session = getSession();
+  return (
+
+    <header className="header">
+      <nav className="header--navbar">
+        <span className="align--span" />
+        <DashboardLink href={session ? '/dashboard' : '/'}>
+          <Image priority className="header--logo" src="/zen-red-logo.png" alt="Logo" width={240} height={66} />
+        </DashboardLink>
+        <Menu className="header--menu" />
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
